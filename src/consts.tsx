72c4b37c9511134af04defe8eb9700e007c923d0
atxrubberroundup <atxrubberroundup@gmail.com>
2024-08-21 16:18:54 -0500

    Updating dates and links! (#1)

--- src/consts.tsx
+++ src/consts.tsx
@@ -1,4 +1,4 @@
 export const eventDates = {
-  start: new Date('2024-02-23T08:00:00-05:00'),
-  end: new Date('2024-02-25T11:59:00-05:00'),
+  start: new Date('2025-02-21T08:00:00-05:00'),
+  end: new Date('2025-02-23T11:59:00-05:00'),
 };
