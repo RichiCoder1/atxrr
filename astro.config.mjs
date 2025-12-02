948b8fb173f22042829c21b30b560256134f6731
Richard Simpson <richardsimpson@outlook.com>
2025-12-02 10:50:55 -0600

    updates pt1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,35 +1,42 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
 import cloudflare from "@astrojs/cloudflare";
-import million from 'million/compiler';
 import react from "@astrojs/react";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
+import million from "million/compiler";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [react(), million.vite({
-		mode: "react",
-		server: true,
-		auto: true
-	}), tailwind({
-		applyBaseStyles: false
-	})],
+	integrations: [
+		react(),
+		million.vite({
+			mode: "react",
+			server: true,
+			auto: true,
+		}),
+	],
 	output: "server",
 	adapter: cloudflare({
 		mode: "directory",
 		platformProxy: {
 			enabled: true,
-		}
+		},
+		imageService: "compile",
 	}),
-	site: process.env.SITE_URL ?? process.env.CF_PAGES_URL ?? 'http://localhost:4321/',
+	site:
+		process.env.SITE_URL ??
+		process.env.CF_PAGES_URL ??
+		"http://localhost:4321/",
 	vite: {
 		server: {
 			proxy: {
-				'^/tt/.*': {
-					target: 'https://cdn.tickettailor.com',
+				"^/tt/.*": {
+					target: "https://cdn.tickettailor.com",
 					changeOrigin: true,
-					rewrite: path => path.replace(/^\/tt/, '')
-				}
-			}
-		}
-	}
+					rewrite: (path) => path.replace(/^\/tt/, ""),
+				},
+			},
+		},
+
+		plugins: [tailwindcss({ applyBaseStyles: false })],
+	},
 });
