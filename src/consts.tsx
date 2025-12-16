953b0fb8a4b01b106d4e7c18a10b2984b716d1b2
moisesm54 <moisesm54@gmail.com>
2025-12-15 23:30:50 -0600

    Update consts.tsx
    Dates for 2026 set!

--- src/consts.tsx
+++ src/consts.tsx
@@ -1,4 +1,5 @@
 export const eventDates = {
-  start: new Date('2025-02-21T08:00:00-05:00'),
-  end: new Date('2025-02-23T11:59:00-05:00'),
+  start: new Date('2026-02-20T08:00:00-05:00'),
+  end: new Date('2026-02-22T11:59:00-05:00'),
 };
+
